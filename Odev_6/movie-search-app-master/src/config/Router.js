a6d699dab53eba80ce5c70602d014af6c3faf5b4
Umut Turan <umutturan107@gmail.com>
2021-06-04 19:52:17 +0300

    React Odev 6 eklendi

---
+++ Odev_6/movie-search-app-master/src/config/Router.js
@@ -0,0 +1,28 @@
+import Home from '../pages/Home';
+import Favorite from '../pages/Favorite';
+import Detail from '../pages/Detail';
+
+export const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: <Home />,
+    title: 'Home',
+    isHeaderElement: true,
+  },
+  {
+    path: '/movies/:id',
+    exact: false,
+    component: <Detail />,
+    title: 'Detail',
+    isHeaderElement: false,
+  },
+  // favorite route'u için config objesi oluşturuldu:
+  {
+    path: '/favorite',
+    exact: false,
+    component: <Favorite />,
+    title: 'Favorite',
+    isHeaderElement: true,
+  }
+];
